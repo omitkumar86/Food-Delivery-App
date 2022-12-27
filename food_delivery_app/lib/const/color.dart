8808784dccc54f062aca93cb82621b992786d1f6
Omit Kumar <omitkumar86@gmail.com>
2022-12-27 20:13:35 +0600

    Update

---
+++ food_delivery_app/lib/const/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/animation.dart';
+
+Color primaryDeepNevi = Color(0xff272B3B);
+Color primaryOrange = Color(0xffF87B36);
+Color seconderyWhite = Color(0xffF5F5F5);
+Color primaryWhite = Color(0xffFFFFFF);
--- food_delivery_app/lib/pages/starting_page.dart
+++ food_delivery_app/lib/pages/starting_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:food_delivery_app/const/color.dart';
 
 class StartingPage extends StatelessWidget {
   const StartingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: primaryDeepNevi,
+    );
   }
 }
